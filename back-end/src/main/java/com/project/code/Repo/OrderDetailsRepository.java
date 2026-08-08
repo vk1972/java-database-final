90c33f5df2307c2414cf228ffc5b836e64ec0595
vk1972 <vladimir.krivak@gmail.com>
2026-08-08 08:55:17 -0400

    Add @Repository annontation to OrderDetailsRepository

--- back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
+++ back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
@@ -3,4 +3,5 @@ package com.project.code.Repo;
 import com.project.code.Model.OrderDetails; 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> { } 
