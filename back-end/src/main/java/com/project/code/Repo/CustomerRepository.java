8397fd7b7ab014a19ed1ff28103cefe6a4277205
vk1972 <vladimir.krivak@gmail.com>
2026-08-08 13:03:30 -0400

    Add import List and repository to  CustomerRepository

--- back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
+++ back-end/src/main/java/com/project/code/Repo/CustomerRepository.java
@@ -2,6 +2,10 @@ package com.project.code.Repo;
 
 import com.project.code.Model.Customer; 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
