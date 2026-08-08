59191b9326c7f67e70492117bb5633b07f64eea0
vk1972 <vladimir.krivak@gmail.com>
2026-08-08 13:08:43 -0400

    Add import List and import stereotype.Repositoryto CustomerRepository

--- back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
+++ back-end/src/main/java/com/project/code/Repo/OrderDetailsRepository.java
@@ -2,6 +2,9 @@ package com.project.code.Repo;
 
 import com.project.code.Model.OrderDetails; 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> { } 
